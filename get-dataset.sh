41cc42d550275c4cf18ce9d0bf45880df44096d4
Arnav <48650781+arnavrneo@users.noreply.github.com>
2023-07-04 11:22:48 +0530

    curl to wget

--- get-dataset.sh
+++ get-dataset.sh
@@ -6,12 +6,10 @@ echo 'Script Working'
 # echo 'Downloading dataset...'
 # curl -L $url1$file -o $file && unzip -q $file -d $dir1
 dir2='./models'
-url2=https://dl.dropboxusercontent.com/s/grgwkhv0zddmlrh
-onnx=/8l-1280-3232.onnx
+url2=https://dl.dropboxusercontent.com/s/grgwkhv0zddmlrh/8l-1280-3232.onnx
 echo 'Downloading onnx model...'
-curl -L $url2$onnx > $dir2$onnx
-url3=https://dl.dropboxusercontent.com/s/dv916v4t5hn6qpu
-ckpt=/torchFlow-ckpt.pt
+wget $url2 -P $dir2
+url3=https://dl.dropboxusercontent.com/s/dv916v4t5hn6qpu/torchFlow-ckpt.pt
 echo 'Downloading yolo checkpoint...'
-curl -L $url3$ckpt > $dir2$ckpt
+wget $url3 -P $dir2
 echo "Downloaded!"
