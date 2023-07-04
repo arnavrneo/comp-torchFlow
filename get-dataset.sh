0aa1748f766226acf1b6802731e3dc3af593967a
Arnav <48650781+arnavrneo@users.noreply.github.com>
2023-07-04 11:12:36 +0530

    Update get-dataset.sh

--- get-dataset.sh
+++ get-dataset.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 echo 'Script Working'
-dir='./dataset'
-url=https://dl.dropboxusercontent.com/s/x919nszlmp62m3r/
+dir1='./dataset'
+url1=https://dl.dropboxusercontent.com/s/x919nszlmp62m3r/
 file='dataset.zip'
 echo 'Downloading dataset...'
-curl -L $url$file -o $file && unzip -q $file -d $dir
+curl -L $url1$file -o $file && unzip -q $file -d $dir1
+dir2='./models'
+url2=https://dl.dropboxusercontent.com/s/grgwkhv0zddmlrh/8l-1280-3232.onnx
+echo 'Downloading onnx model...'
+curl -L $url2 -d $dir2
+url3=https://dl.dropboxusercontent.com/s/dv916v4t5hn6qpu/torchFlow-ckpt.pt
+echo 'Downloading yolo checkpoint...'
+curl -L $url3 -d $dir2
 echo "Downloaded!"
