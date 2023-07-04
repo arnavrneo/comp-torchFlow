f96067b89086b4c279db6de71f7bffc857d5ffc5
Arnav <48650781+arnavrneo@users.noreply.github.com>
2023-07-04 13:01:07 +0530

    Update get-dataset.sh

--- get-dataset.sh
+++ get-dataset.sh
@@ -3,16 +3,16 @@ echo 'Script Working'
 dir1='./dataset/'
 dataset=https://www.dropbox.com/s/3k2pt9qh36vcvqb/
 name1=dataset-original.zip
-wget $dataset$name1 -P $dir1 && unzip $dir1$name1 && rm $name1
+wget $dataset$name1 -P $dir1 && unzip $dir1$name1 && rm $dir1$name1
 dataset256=https://www.dropbox.com/s/9bvxp36g13li63w/
 name2=dataset-256.zip
-wget $dataset256$name2 -P $dir1 && unzip $dir1$name2 && rm $name2
+wget $dataset256$name2 -P $dir1 && unzip $dir1$name2 && rm $dir$name2
 dataset512=https://www.dropbox.com/s/q4rdb0g26j107m9/
 name3=dataset-512.zip
-wget $dataset512$name3 -P $dir1 && unzip $dir1$name3 && rm $name3
+wget $dataset512$name3 -P $dir1 && unzip $dir1$name3 && rm $dir$name3
 dataset1280=https://www.dropbox.com/s/9totbwbdnm12jf7/
 name4=dataset-1280.zip
-wget $dataset1280$name4 -P $dir1 && unzip $dir1$name4 && rm $name4
+wget $dataset1280$name4 -P $dir1 && unzip $dir1$name4 && rm $dir$name4
 echo 'Downloading dataset...'
 dir2='./models'
 url2=https://dl.dropboxusercontent.com/s/grgwkhv0zddmlrh/8l-1280-3232.onnx
