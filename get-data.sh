d56bf4d57b5a076d21bdac8b96359eba22ae95a3
Arnav <48650781+arnavrneo@users.noreply.github.com>
2023-07-04 22:09:47 +0530

    Update get-data.sh

--- get-data.sh
+++ get-data.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 echo 'Script Working'
 dir1='./dataset/'
-dataset=https://www.dropbox.com/s/3k2pt9qh36vcvqb/
+dataset=https://dl.dropboxusercontent.com/s/2a9jvq1m4vhp8rp/
 name1=dataset-original.zip
 wget $dataset$name1 -P $dir1 && unzip $dir1$name1 && rm $dir1$name1
-dataset256=https://www.dropbox.com/s/9bvxp36g13li63w/
+dataset256=https://dl.dropboxusercontent.com/s/9bvxp36g13li63w/
 name2=dataset-256.zip
 wget $dataset256$name2 -P $dir1 && unzip $dir1$name2 && rm $dir$name2
-dataset512=https://www.dropbox.com/s/q4rdb0g26j107m9/
+dataset512=https://dl.dropboxusercontent.com/s/q4rdb0g26j107m9/
 name3=dataset-512.zip
 wget $dataset512$name3 -P $dir1 && unzip $dir1$name3 && rm $dir$name3
-dataset1280=https://www.dropbox.com/s/9totbwbdnm12jf7/
+dataset1280=https://dl.dropboxusercontent.com/s/9totbwbdnm12jf7/
 name4=dataset-1280.zip
 wget $dataset1280$name4 -P $dir1 && unzip $dir1$name4 && rm $dir$name4
 echo 'Downloading dataset...'
