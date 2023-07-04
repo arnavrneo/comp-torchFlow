c590be9290e48fafdeadbde84c8cb30ff8f75617
Arnav <48650781+arnavrneo@users.noreply.github.com>
2023-07-04 11:18:42 +0530

    Update get-dataset.sh

--- get-dataset.sh
+++ get-dataset.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 echo 'Script Working'
-dir1='./dataset'
-url1=https://dl.dropboxusercontent.com/s/x919nszlmp62m3r/
-file='dataset.zip'
-echo 'Downloading dataset...'
-curl -L $url1$file -o $file && unzip -q $file -d $dir1
+# dir1='./dataset'
+# url1=https://dl.dropboxusercontent.com/s/x919nszlmp62m3r/
+# file='dataset.zip'
+# echo 'Downloading dataset...'
+# curl -L $url1$file -o $file && unzip -q $file -d $dir1
 dir2='./models'
-url2=https://dl.dropboxusercontent.com/s/grgwkhv0zddmlrh/8l-1280-3232.onnx
+url2=https://dl.dropboxusercontent.com/s/grgwkhv0zddmlrh
+onnx=/8l-1280-3232.onnx
 echo 'Downloading onnx model...'
-curl -L $url2 -d $dir2
-url3=https://dl.dropboxusercontent.com/s/dv916v4t5hn6qpu/torchFlow-ckpt.pt
+curl -L $url2$onnx > $dir2$onnx
+url3=https://dl.dropboxusercontent.com/s/dv916v4t5hn6qpu
+ckpt=/torchFlow-ckpt.pt
 echo 'Downloading yolo checkpoint...'
-curl -L $url3 -d $dir2
+curl -L $url3$ckpt > $dir2$ckpt
 echo "Downloaded!"
