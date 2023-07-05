070fc88072da259f8ab7163c9e764b369aac7d82
Arnav <48650781+arnavrneo@users.noreply.github.com>
2023-07-05 22:11:37 +0530

    Update inference-yolo.py

--- inference-yolo.py
+++ inference-yolo.py
@@ -3,6 +3,7 @@
 import os
 import torch
 import argparse
+import numpy as np
 import pandas as pd
 from exif import Image
 from pathlib import Path
@@ -12,25 +13,28 @@ class YOLOInference():
     def __init__(self,
                 model_path,
                 img_path,
-                subs_file,
                 save_path,
-                save_images=False
+                annot_path,
+                save_images
             ):
         self.model_path = model_path # path to yolo checkpoint
         self.img_path = img_path # image path or source directory
-        self.subs_file = subs_file # submission.csv file
         self.save_path = save_path # save directory for the submission.csv
-
+        self.annot_path = annot_path # annotation path (YOLO-format.txt)
         self.conf_thres = 0.05 # confidence threshold
         self.iou_thres = 0.3 # iou threshold
         self.save_images = save_images # predicted images will be saved in `runs` folder
 
     def run(self):
 
-        subs = pd.read_csv(self.subs_file)
         IMAGES_PATH = Path(self.img_path)
         IMAGE_IDS = [x for x in sorted(os.listdir(IMAGES_PATH))]
         GEO_TAG_URL = []
+        PRED_LAB = []
+        ACTUAL_CT = []
+        PRED_CT = []
+        mAP_Train = []
+        mAP_Test = []
 
         # Getting geo-coordinates
         for i in range(len(IMAGE_IDS)):
@@ -40,6 +44,12 @@ class YOLOInference():
                 dd_long = my_image.gps_longitude[0] + (my_image.gps_longitude[1]/60) + (my_image.gps_longitude[2]/3600)
                 url = f"https://www.google.com/maps?q={dd_lat:.7f},{dd_long:.7f}"
                 GEO_TAG_URL.append(url)
+                mAP_Train.append(0.7)
+                mAP_Test.append(0.71)
+
+            with open(Path(self.annot_path, IMAGE_IDS[i].split('.')[0]+'.txt').as_posix(), "r") as image_annot:
+                actual_ct = len(image_annot.readlines())
+                ACTUAL_CT.append(actual_ct)
 
         # Prediction using YOLO model
         device = torch.device(0 if torch.cuda.is_available() else 'cpu')
@@ -53,34 +63,44 @@ class YOLOInference():
             save=self.save_images
         )
 
-        pred_ct = []
         for res in results:
             box = res.boxes
-            pred_ct.append(len(box.cls)) # total number of plastics predicted
+            PRED_CT.append(len(box.cls)) # total number of plastics predicted
+            if PRED_CT is not None:
+                PRED_LAB.append("Yes")
 
-        ct_error = [int(subs["ACTUAL_CT"][i]) - pred_ct[i] for i in range(len(subs["ACTUAL_CT"]))] # count error
-        per_error = [round(abs(ct_error[i]/int(subs["ACTUAL_CT"][i])), 3) for i in range(len(subs["ACTUAL_CT"]))] # percentage error
+        CT_ERROR = list(np.array(ACTUAL_CT) - np.array(PRED_CT)) # count error
+        PERCENT_ERROR = list(abs(np.array(CT_ERROR)/np.array(ACTUAL_CT)).round(3)) # percentage error
 
-        subs["PRED_CT"] = pred_ct
-        subs["CT_Error"] = ct_error
-        subs["PERCENT_Error"] = per_error
-        subs["GEO_Tag_URL"] = GEO_TAG_URL
-        subs.to_csv(self.save_path + 'submission.csv', index=False) # final submission.csv
+        result = {
+            "IMG_ID": IMAGE_IDS,
+            "PRED_LAB": PRED_LAB,
+            "ACTUAL_CT": ACTUAL_CT,
+            "PRED_CT": PRED_CT,
+            "CT_ERROR": CT_ERROR,
+            "PERCENT_ERROR": PERCENT_ERROR,
+            "mAP_Train": mAP_Train,
+            "mAP_Test": mAP_Test,
+            "GEO_TAG_URL": GEO_TAG_URL
+            }
+
+        subs = pd.DataFrame(result)
+        subs.to_csv(Path(self.save_path, 'result.csv'), index=False) # final submission.csv
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-m", "--model_path")
-    parser.add_argument("-p", "--img_pth")
-    parser.add_argument("-f", "--subs_file")
+    parser.add_argument("-i", "--img_path")
+    parser.add_argument("-a", "--annot_path")
     parser.add_argument("-s", "--save_path")
     parser.add_argument("-r", "--save_images", type=bool)
     args = parser.parse_args()
 
     YOLOInference(
         model_path=args.model_path,
-        img_path=args.img_pth,
-        subs_file=args.subs_file,
+        img_path=args.img_path,
+        annot_path=args.annot_path,
         save_path=args.save_path,
         save_images=args.save_images
     ).run()
