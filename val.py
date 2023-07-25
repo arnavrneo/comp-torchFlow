28063c97ccaf1d973a2fa75564b2c3333a0713df
arnavrneo <arnavraina02@gmail.com>
2023-07-25 14:04:57 +0530

    revert this back

--- val.py
+++ val.py
@@ -15,7 +15,14 @@ def cfg2dict(cfg):
         cfg = vars(cfg)  # convert to dict
     return cfg
 
-def val(ckpt, cfg="config/val-config.yaml"):
+def val(
+        ckpt,
+        imgsz,
+        batch,
+        conf,
+        iou,
+        cfg="config/val-config.yaml"
+    ):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Running on: {device}")
 
@@ -25,14 +32,35 @@ def val(ckpt, cfg="config/val-config.yaml"):
     args = cfg2dict(cfg)
 
     model.val(
-        **args
+        data="config/dataset.yaml",
+        imgsz=imgsz, # [640, 480]
+        batch=batch,
+        save_json=True,
+        save_hybrid=False,
+        conf=conf,
+        iou=iou,
+        max_det=300,
+        half=True,
+        device=0,
+        dnn=False,
+        plots=False,
+        rect=False,
+        split="val"
     )
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-m", "--ckpt")
+    parser.add_argument("-s", "--img_size", type=int)
+    parser.add_argument("-b", "--batch_size", type=int)
+    parser.add_argument("-c", "--conf", type=float)
+    parser.add_argument("-i", "--iou", type=float)
     args = parser.parse_args()
 
     val(
-        ckpt=args.ckpt
+        ckpt=args.ckpt,
+        imgsz=args.img_size,
+        batch=args.batch_size,
+        conf=args.conf,
+        iou=args.iou
     )
