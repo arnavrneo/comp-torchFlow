cdb885142a07311f0dd6e3bb3e8820f1d3fa5528
arnavrneo <arnavraina02@gmail.com>
2023-07-26 11:16:17 +0530

    update

--- val.py
+++ val.py
@@ -17,10 +17,6 @@ def cfg2dict(cfg):
 
 def val(
         ckpt,
-        imgsz,
-        batch,
-        conf,
-        iou,
         cfg="config/val-config.yaml"
     ):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -32,35 +28,14 @@ def val(
     args = cfg2dict(cfg)
 
     model.val(
-        data="config/dataset.yaml",
-        imgsz=imgsz, # [640, 480]
-        batch=batch,
-        save_json=True,
-        save_hybrid=False,
-        conf=conf,
-        iou=iou,
-        max_det=300,
-        half=True,
-        device=0,
-        dnn=False,
-        plots=False,
-        rect=False,
-        split="val"
+        **args
     )
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-m", "--ckpt")
-    parser.add_argument("-s", "--img_size", type=int)
-    parser.add_argument("-b", "--batch_size", type=int)
-    parser.add_argument("-c", "--conf", type=float)
-    parser.add_argument("-i", "--iou", type=float)
     args = parser.parse_args()
 
     val(
-        ckpt=args.ckpt,
-        imgsz=args.img_size,
-        batch=args.batch_size,
-        conf=args.conf,
-        iou=args.iou
+        ckpt=args.ckpt
     )
